9c4f205da6be479cf2ac3d716b107616597f4c7c
BartBurton <bburtonZero@gmail.com>
2021-03-26 04:11:14 +0300

    BookShop_1.2

--- BooksShop/BooksShop/Controllers/ExcelToFromDbController.cs
+++ BooksShop/BooksShop/Controllers/ExcelToFromDbController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using BooksShop.Models;
+using Excel;
 using ClosedXML.Excel;
 
 namespace BooksShop.Controllers
@@ -37,16 +38,51 @@ namespace BooksShop.Controllers
         [HttpPost]
         public IActionResult Load()
         {
-            //Если файл загружен
-            if(Request.Form.Files.Count != 0)
+            //Если файл не загружен
+            if (Request.Form.Files.Count == 0) return RedirectToAction("Excel");
+            
+            //Обноляем БД
+            var file = Request.Form.Files.First();
+            List<object[]> body = ExcelFileInteraction.FromExcel(file.OpenReadStream());
+
+            Book book = null;
+            Autor autor = null;
+            BooksAutor ba;
+            foreach (var row in body)
             {
-                var file = Request.Form.Files.First();
-                //Обноляет БД
-                ExcelFileToDb(file);
-                return RedirectToAction("Autors", "Autors");
+                //Читаем данные для книги если, есть название
+                //Если авторов книги несколько, экземпляр создастся один раз
+                if (row[0].ToString() != "")
+                {
+                    book = new Book()
+                    {
+                        Title = row[0].ToString(), Edition = row[1].ToString(),
+                        PublishedAt = row[2].ToString(), Description = row[3].ToString()
+                    };
+                    _bs.Books.Add(book);
+                }
+
+                //Авторы связываются с текущим экземпляром book
+                //Если у книги нет автора или строка пустая автор не создается
+                if (row[4].ToString() != "")
+                {
+                    autor = new Autor() { Name = row[4].ToString(), Dob = row[5] as DateTime? };
+                    _bs.Autors.Add(autor);
+                }
+
+                //Связывание проиходит только, если оба были загружены
+                if (autor != null && book != null)
+                {
+                    ba = new BooksAutor() { IdAutorNavigation = autor, IdBookNavigation = book };
+                    _bs.BooksAutors.Add(ba);
+                }
+
+                _bs.SaveChanges();
+                //Если книга без автора нужно предотвратить с предыдущим автором.
+                autor = null;
             }
 
-            return RedirectToAction("Excel");
+            return RedirectToAction("Autors", "Autors");
         }
 
         /// <summary>
@@ -60,143 +96,35 @@ namespace BooksShop.Controllers
                     .ThenInclude(ba => ba.IdAutorNavigation)
                 .ToListAsync();
 
-            //Возвращаем файл excel
-            return DbToExcelFile(books);
-        }
-
-
-
-        #region Работа с excel файлом
-
-        /// <summary>
-        /// Загрузить данные из БД в excel файл.
-        /// </summary>
-        /// <param name="books">Список книг, с всеми связанными сущностями</param>
-        /// <returns>Файл для скачивания.</returns>
-        private FileResult DbToExcelFile(List<Book> books)
-        {
-            //Создаем рабочую область
-            var workbook = new XLWorkbook();
-            var sheet = workbook.Worksheets.Add("Books Shop");
-
-            //Устанавливаем загаловки таблицы
-            sheet.Cell("A2").Value = "Название";
-            sheet.Cell("B2").Value = "Издание";
-            sheet.Cell("C2").Value = "Издатель";
-            sheet.Cell("D2").Value = "Описание";
-            sheet.Cell("E2").Value = "Автор";
-            sheet.Cell("F2").Value = "Дата рождения автора";
-            sheet.Range("A2:F2").Style.Fill.BackgroundColor = XLColor.FromArgb(230, 184, 183);
-            sheet.Row(2).Style.Font.SetFontSize(18);
-            sheet.Row(2).Style.Font.SetBold(true);
-
-            //Начинаем записывать данные с 3 строки, т.к. в первой
-            //кол-во строк с данными, во второй заголовки
-            long count = 3;
+            List<object[]> body = new List<object[]>();
+            body.Add(new object[6]);
             foreach (var book in books)
             {
-                sheet.Cell("A" + count).Value = book.Title;
-                sheet.Cell("B" + count).Value = book.Edition;
-                sheet.Cell("C" + count).Value = book.PublishedAt;
-                sheet.Cell("D" + count).Value = book.Description;
-                foreach (var autor in book.BooksAutors)
-                {
-                    //Данные об авторах книги записываются без дублирования данных
-                    //о книге, но на новой строки, поэтому count растет
-                    sheet.Cell("E" + count).Value = autor.IdAutorNavigation.Name;
-                    sheet.Cell("F" + count).Value = autor.IdAutorNavigation.Dob?.ToString("d");
-                    count++;
+                body.Last()[0] = book.Title;
+                body.Last()[1] = book.Edition;
+                body.Last()[2] = book.PublishedAt;
+                body.Last()[3] = book.Description;
+                foreach (var ba in book.BooksAutors)
+                { 
+                    body.Last()[4] = ba.IdAutorNavigation.Name;
+                    body.Last()[5] = ba.IdAutorNavigation.Dob;
+                    body.Add(new object[6]);
                 }
-                //Если у книги нет авторов count не инткрементировался в цикле
-                count += (book.BooksAutors.Count == 0) ? 1 : 0;
+                if(book.BooksAutors.Count == 0) body.Add(new object[6]);
             }
+            body.RemoveAt(body.Count - 1);
 
-            //Кол-во строк с данными
-            sheet.Cell("A1").Value = count - 3;
-            sheet.Cell("B1").Value = "Кол-во строк с данными";
-            //Стиль содержания таблицы
-            sheet.Range("A2:F" + (count - 1)).Style.Border.InsideBorder = XLBorderStyleValues.Thin;
-            sheet.Range("A2:F" + (count - 1)).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-            sheet.Range("A2:F2").Style.Border.BottomBorder = XLBorderStyleValues.Medium;
-            sheet.Range("A3:F" + count).Style.Fill.BackgroundColor = XLColor.FromArgb(216, 228, 188);
-            sheet.Range("A3:F" + count).Style.Font.SetFontSize(14);
-            sheet.Columns().AdjustToContents();
-
-            //Возврат файла на скачивание
-            using (MemoryStream stream = new MemoryStream())
+            string[] headers = new string[6] 
             {
-                workbook.SaveAs(stream);
-                return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Books Shop.xlsx");
-            }
+                "Название", "Издание", "Издатель", "Описание", "Автор", "Дата рождения автора"
+            };
+
+            //Возвращаем файл excel
+            return File(
+                ((MemoryStream)ExcelFileInteraction.ToExcel("Books Shop", headers, body, 
+                new StyleSheet() { HeaderBackgroundColor = (230, 184, 183), BodyBackgroundColor = (216, 228, 188) }))
+                .ToArray(), 
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Books Shop.xlsx");
         }
-
-        /// <summary>
-        /// Загрузить данные из файла в базу данных
-        /// </summary>
-        /// <param name="file">Файл полученный из формы</param>
-        private void ExcelFileToDb(IFormFile file)
-        {
-            //Считываем поток файла, открываем первую страницу
-            var workbook = new XLWorkbook(file.OpenReadStream());
-            var sheet = workbook.Worksheet(1);
-
-            int count = 0;
-            try
-            {
-                //В ячейке A1 указано кол-во строк с данными
-                count = Convert.ToInt32(sheet.Cell(1, 1).Value);
-            }
-            catch { return; }
-
-            //Считывание с 3 строки, т.к. во 2 заголовки таблицы
-            var rows = sheet.RangeUsed().Rows(3, 2 + count);
-
-            Book book = null;
-            Autor autor = null;
-            BooksAutor ba;
-            foreach (var row in rows)
-            {
-                //Читаем данные для книги если, есть название
-                //Если авторов книги несколько, экземпляр создастся один раз
-                if (row.Cell(1).Value.ToString() != "")
-                {
-                    book = new Book()
-                    {
-                        Title = row.Cell(1).Value.ToString(),
-                        Edition = row.Cell(2).Value.ToString(),
-                        PublishedAt = row.Cell(3).Value.ToString(),
-                        Description = row.Cell(4).Value.ToString()
-                    };
-                    _bs.Books.Add(book);
-                }
-
-                //Авторы связываются с текущим экземпляром book
-                //Если у книги нет автора или строка пустая автор не создается
-                if (row.Cell(5).Value.ToString() != "")
-                {
-                    autor = new Autor()
-                    {
-                        Name = row.Cell(5).Value.ToString(),
-                        Dob = row.Cell(6).Value as DateTime?
-                    }; 
-                    _bs.Autors.Add(autor);
-                }
-
-                //Связывание проиходит только, если оба были загружены
-                if (autor != null && book != null)
-                {
-                    ba = new BooksAutor()
-                    {
-                        IdAutorNavigation = autor,
-                        IdBookNavigation = book
-                    }; 
-                    _bs.BooksAutors.Add(ba);
-                }
-
-                _bs.SaveChanges();
-            }
-        } 
-
-        #endregion
     }
 }
---
+++ BooksShop/ExcelFileInteraction/ExcelFileInteraction.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+
+namespace Excel
+{
+    /// <summary>
+    /// Задает настройки стиля генерируемой таблицы.
+    /// </summary>
+    public class StyleSheet
+    {
+        /// <summary>
+        /// Цвет фона заголовка таблицы.
+        /// </summary>
+        public (byte r, byte g, byte b) HeaderBackgroundColor = (255, 255, 255);
+        /// <summary>
+        /// Цвет шрифта заголовка таблицы.
+        /// </summary>
+        public (byte r, byte g, byte b) HeaderFontColor = (0, 0, 0);
+        /// <summary>
+        /// Размер шрифта заголовка таблицы.
+        /// </summary>
+        public double FontSizeHeader = 20;
+        /// <summary>
+        /// Определяет шрифт заголовка таблицы жирным.
+        /// </summary>
+        public bool HeaderIsBold = true;
+        /// <summary>
+        /// Определяет нижнюю границу заголовка таблицы жирной.
+        /// </summary>
+        public bool HeaderBottomBorderIsBold = true;
+
+        /// <summary>
+        /// Цвет фона тела таблицы.
+        /// </summary>
+        public (byte r, byte g, byte b) BodyBackgroundColor = (255, 255, 255);
+        /// <summary>
+        /// Цвет шрифта тела таблицы.
+        /// </summary>
+        public (byte r, byte g, byte b) BodyFontColor = (0, 0, 0);
+        /// <summary>
+        /// Размер шрифта тела таблицы.
+        /// </summary>
+        public double FontSizeBody = 15;
+
+        /// <summary>
+        /// Определяет внутренние границы границы таблицы.
+        /// </summary>
+        public bool InsideBorder = true;
+        /// <summary>
+        /// Определяет внешние границы границы таблицы.
+        /// </summary>
+        public bool OutsideBorder = true;
+
+        /// <summary>
+        /// Устанавливает стиль указанной области.
+        /// </summary>
+        /// <param name="style">Стиль</param>
+        /// <param name="sheet">Рабочий лист</param>
+        /// <param name="firsRow">Первая строка стилизируемой области</param>
+        /// <param name="firstColumn">Первый столбец стилизируемой области</param>
+        /// <param name="lastRow">Последняя строка стилизируемой области</param>
+        /// <param name="lastColumn">Последний столбец стилизируемой области</param>
+        internal static void SetStyleSheet(StyleSheet style, ref IXLWorksheet sheet, int firsRow, int firstColumn, int lastRow, int lastColumn)
+        {
+            //Стили для всей таблицы
+            sheet.Range(firsRow, firstColumn, lastRow, lastColumn).Style.Border.InsideBorder =
+                (style.InsideBorder) ? XLBorderStyleValues.Thin : XLBorderStyleValues.None;
+
+            sheet.Range(firsRow, firstColumn, lastRow, lastColumn).Style.Border.OutsideBorder =
+                (style.OutsideBorder) ? XLBorderStyleValues.Thin : XLBorderStyleValues.None;
+
+            sheet.Range(firsRow, firstColumn, firsRow, lastColumn).Style.Border.BottomBorder =
+                (style.HeaderBottomBorderIsBold) ? XLBorderStyleValues.Medium : XLBorderStyleValues.None;
+
+            //Стили для заголовков таблицы
+            sheet.Range(firsRow, firstColumn, firsRow, lastColumn).Style.Fill.BackgroundColor
+                = XLColor.FromArgb(style.HeaderBackgroundColor.r, style.HeaderBackgroundColor.g, style.HeaderBackgroundColor.b);
+
+            sheet.Range(firsRow, firstColumn, firsRow, lastColumn).Style.Font.FontColor
+                = XLColor.FromArgb(style.HeaderFontColor.r, style.HeaderFontColor.g, style.HeaderFontColor.b);
+
+            sheet.Row(firsRow).Style.Font.SetFontSize(style.FontSizeHeader);
+
+            sheet.Row(firsRow).Style.Font.SetBold(style.HeaderIsBold);
+
+
+            //Стили для тела таблицы
+            sheet.Range(firsRow + 1, firstColumn, lastRow, lastColumn).Style.Fill.BackgroundColor
+                = XLColor.FromArgb(style.BodyBackgroundColor.r, style.BodyBackgroundColor.g, style.BodyBackgroundColor.b);
+
+            sheet.Range(firsRow + 1, firstColumn, lastRow, lastColumn).Style.Font.FontColor
+                = XLColor.FromArgb(style.BodyFontColor.r, style.BodyFontColor.g, style.BodyFontColor.b);
+
+            sheet.Range(firsRow + 1, firstColumn, lastRow, lastColumn).Style.Font.SetFontSize(style.FontSizeBody);
+        }
+    }
+
+
+    /// <summary>
+    /// Исключение генерируемое при не совпадении кол-ва заголовков и столюцов тела таблицы.
+    /// </summary>
+    public class HeaderNotEqualBodyException : Exception
+    {
+        public HeaderNotEqualBodyException() { }
+        public override string Message => "Количество заголовкав не равно количеству столбцов тела таблицы!";
+    }
+
+    /// <summary>
+    /// Исключение генерируемое при не соблюдении соглашения о формате таблицы файла Excel.
+    /// </summary>
+    public class InvalidSizeTableException : Exception
+    {
+        public InvalidSizeTableException() { }
+        public override string Message => "Ячейка А1 не содержит количество строк данных таблицы или ячейка B1 " +
+            "не содержит количество столбцов таблицы";
+    }
+
+    /// <summary>
+    /// Реализует взаимодействие с файлом Excel.
+    /// </summary>
+    public static class ExcelFileInteraction
+    {
+        /// <summary>
+        /// Номер строки, с которой начинается запись данных.
+        /// </summary>
+        private const int BODY_START = 3;
+
+        /// <summary>
+        /// Преобразует данные в поток для Excel файла.
+        /// </summary>
+        /// <param name="nameSheet">Имя листа</param>
+        /// <param name="headers">Массив заголовков таблицы</param>
+        /// <param name="body">Двумерный список данных для таблицых</param>
+        /// <param name="style">Стиль генерируемой таблицы</param>
+        /// <returns>Поток для Excel файла</returns>
+        /// <exception cref="HeaderNotEqualBodyException">Если количество заголовков не совпадает с количеством столбцов данных таблицы.</exception>
+        public static Stream ToExcel(string nameSheet, string[] headers, List<object[]> body, StyleSheet style)
+        {
+            if (headers.Length != body.First().Length) { throw new HeaderNotEqualBodyException(); }
+
+            //Создаем рабочую область
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(nameSheet);
+
+            //Настройка стилей
+            StyleSheet.SetStyleSheet(style, ref sheet, BODY_START - 1, 1, body.Count + (BODY_START - 1), headers.Length);
+
+            //Размеры телы таблицы
+            sheet.Cell("A1").Value = $"Строк данных - {body.Count}";
+            sheet.Cell("B1").Value = $"Столбцов данных - {headers.Length}";
+
+            //Устанавливаем загаловки таблицы
+            for (int i = 0; i < headers.Length; i++) 
+            { 
+                sheet.Cell(BODY_START - 1, i + 1).Value = headers[i]; 
+            }
+
+            //Начинаем записывать данные с 3 строки, т.к. в первой
+            //кол-во строк с данными, во второй заголовки
+            int countRow = BODY_START;
+            foreach (var row in body)
+            {
+                for (int i = 0; i < row.Length; i++)
+                {
+                    sheet.Cell(countRow, i + 1).Value = row[i];
+                }
+                countRow++;
+            }
+            sheet.Columns().AdjustToContents();
+
+
+            //Возврат потока файла
+            using (MemoryStream stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                return stream;
+            }
+        }
+
+
+        /// <summary>
+        /// Преобразует данные из потока Excel файла в таблицу данных.
+        /// </summary>
+        /// <param name="file">Поток Excel файла. Ожидается, во второй строке расположены заголовки таблицы поэтому она пропускается.</param>
+        /// <returns>Двумерный список данных из полученной таблицы файла</returns>
+        /// <exception cref="InvalidSizeTableException">Если в ячейке А1 не указано количество строк данных или если в ячейке B1 не указано количество столбцов данных</exception>
+        public static List<object[]> FromExcel(Stream file)
+        {
+            //Считываем поток файла, открываем первую страницу
+            var workbook = new XLWorkbook(file);
+            var sheet = workbook.Worksheet(1);
+
+            uint countRow = 0, countCol = 0;
+            try
+            {
+                countRow = (uint)Convert.ToInt32(sheet.Cell("A1").Value);
+                countCol = (uint)Convert.ToInt32(sheet.Cell("B1").Value);
+            }
+            catch { throw new InvalidSizeTableException(); }
+
+            //Считывание с 3 строки, т.к. во 2 заголовки таблицы, в 1 данные о размере
+            var rows = sheet.RangeUsed().Rows(BODY_START, (int)countRow + (BODY_START - 1));
+            List<object[]> data = new List<object[]>();
+            foreach (var row in rows)
+            {
+                data.Add(new object[countCol]);
+                for (int i = 0; i < data.Last().Length; i++)
+                {
+                    data.Last()[i] = row.Cell(i + 1).Value;
+                }
+            }
+            return data;
+        }
+    }
+}
